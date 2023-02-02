5259665028d224f43ad8c758840940c92c8993c5
bdegrieck <bendegrieck@comcast.net>
2023-02-02 18:45:51 -0500

    main class and the other

---
+++ App.java
@@ -0,0 +1,16 @@
+import Course.Course;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        int courseNum = 0;
+        String courseName = "";
+
+        Course[] bioengineeringReq = new Course[50];
+        bioengineeringReq[0] = new Course("APM", 2663);
+    }
+
+}
---
+++ Course.java
@@ -0,0 +1,15 @@
+package Course;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Course {
+    int courseNumber;
+    String courseName;
+
+    public Course(String name, int num) {
+        courseNumber = num;
+        courseName = name;
+    }
+
+}
